72aa922d6c751d83199961fef01bbd9d84c86714
devopcasting <devopcasting@gmail.com>
2024-07-15 20:03:49 +0530

    added the logic of embedding

--- app/upload/routes.py
+++ app/upload/routes.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 from app import app, db
 from app.upload.forms import UploadForm
 from app.models import Document
+from app.upload.doc_pdf.embedding import ProcessPDFDcoument
 
 # Create a Blueprint instance for the 'upload' module
 upload_route = Blueprint('upload', __name__, template_folder='templates')
@@ -47,9 +48,29 @@ def get_pdf_file_size(file_path) -> str:
 
     return file_size
 
+# Identify the document type
+def identify_document_type(file_path) -> str:
+    # Use the file extension to identify the document type
+    file_extension = os.path.splitext(file_path)[1][1:].lower()
+    if file_extension == 'pdf':
+        return 'PDF'
+    elif file_extension in ['md', 'txt', 'docx', 'html', 'csv', 'xls', 'xlsx']:
+        return 'Text'
+    else:
+        return 'Unknown'
+    
 # Add a route for the upload page
 @upload_route.route('/upload', methods=['GET', 'POST'])
 def upload():
+    # Pagination
+    page = request.args.get('page', 1, type=int)
+    # Total documents uploaded
+    total_documents = Document.query.count()
+    # Documents per page
+    documents_per_page = 5
+    # Get the documents for the current page
+    documents = Document.query.order_by(Document.id.desc()).paginate(page=page, per_page=documents_per_page, error_out=False)
+
     form = UploadForm()
     if form.validate_on_submit():
         # Get the uploaded file from the form
@@ -63,11 +84,21 @@ def upload():
             file.seek(0)
             document = Document.query.filter_by(md5sum=md5sum).first()
             if document:
-                flash(f"File already exists! '{file_name}'", 'danger')
+                flash(f"Document already exists! '{file_name}'", 'danger')
                 return redirect(request.url)
             # Save the file to the upload folder
             file.save(file_path)
 
+            # Identify the document type
+            document_type = identify_document_type(file_path)
+            # Case for document type
+            match document_type:
+                case 'PDF':
+                    # Process the PDF document
+                    process_pdf = ProcessPDFDcoument(file_path, VECTOR_DB_PATH)
+                    if not process_pdf.embed_doc():
+                        flash(f"Failed to process PDF document! '{file_name}'", 'danger')
+                        return redirect(request.url)
             # Get the document size
             document_size = get_pdf_file_size(file_path)
            
@@ -77,9 +108,9 @@ def upload():
             db.session.commit()
 
             # Redirect to the home page after successful upload
-            flash(f"File uploaded successfully! '{file_name}'", 'success')
+            flash(f"Document uploaded successfully! '{file_name}'", 'success')
             return redirect(url_for('upload.upload'))
         else:
-            flash(f"File type not allowed! '{file_name}'", 'danger')
+            flash(f"Document type not allowed! '{file_name}'", 'danger')
             return redirect(request.url)
-    return render_template('upload/upload.html', title='Upload', form=form)
+    return render_template('upload/upload.html', title='Upload', form=form, documents=documents, total_documents=total_documents)
