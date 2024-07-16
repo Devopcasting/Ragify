d130aa22e175874150c75b0146d9b672c4599cfa
devopcasting <devopcasting@gmail.com>
2024-07-16 15:36:43 +0530

    updated logic

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # Configure app instance
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 # Set static folder path
 static_folder_path = os.path.join(app.root_path, 'static')
@@ -20,7 +20,10 @@ app.static_folder = static_folder_path
 
 # Create SQLite instance
 db = SQLAlchemy(app)
+
+# Import models
 from app.models import Document
+
 # Create the context
 with app.app_context():
     # Create the database tables
@@ -29,5 +32,7 @@ with app.app_context():
 # Register blueprints here
 from app.home.routes import home_route
 from app.upload.routes import upload_route
+from app.query.routes import query_route
 app.register_blueprint(home_route)
 app.register_blueprint(upload_route)
+app.register_blueprint(query_route)
